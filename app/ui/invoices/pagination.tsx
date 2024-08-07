cc1be12dfe5daf34d28d7d22ace04315e6a99cd9
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-08-08 02:00:56 +0800

    Update pagination.tsx

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,6 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
    const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
   
   const createPageUrl = (pageNumber : number | string) => {
 	const params = new URLSearchParams(searchParams);
