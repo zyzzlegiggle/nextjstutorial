0ff7acf22151852842950dbbe8cf21cecfdc4223
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-08-08 01:38:30 +0800

    a

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -22,6 +22,7 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+ 
   const totalPages = await fetchInvoicesPages(query);
  
   return (
@@ -37,9 +38,8 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-         <Pagination totalPages={totalPages} /> 
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
-  //a
 }
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -17,6 +17,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 	const params = new URLSearchParams(searchParams);
 	params.set('page', pageNumber.toString());
 	return `${pathname}?${params.toString()}`;
+	};
 
   return (
     <>
