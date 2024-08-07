b08e1cdf5197dec9b531c18bd44f6abf0ec634e3
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-08-07 16:47:34 +0800

    Update action.ts

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,9 +2,9 @@
 
 export async function createInvoice(formData : formData){
 	const rawFormData = {
-		customerId: formData.get('customerId');
-		amount: formData.get('amount');
-		status: formData.get('status');
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
 	};
 	console.log(rawFormData);
 }
