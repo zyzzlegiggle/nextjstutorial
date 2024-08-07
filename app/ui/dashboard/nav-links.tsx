1e4ccc5f7630f71521e9a013606ff86f3eb4487f
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-08-07 12:14:48 +0800

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/Link';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
