6c5cbb6ea2e6ef86d6468e48501b99a05ef2a211
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-08-07 16:46:08 +0800

    a

---
+++ app/lib/action.ts
@@ -0,0 +1,10 @@
+'use server';
+
+export async function createInvoice(formData : formData){
+	const rawFormData = {
+		customerId: formData.get('customerId');
+		amount: formData.get('amount');
+		status: formData.get('status');
+	};
+	console.log(rawFormData);
+}
